09a759e0004a98570392311efb2e7be28181ac91
Rupak Sil <rupaksil007@gmail.com>
2026-06-26 00:21:23 +0530

    commit

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { dev, isServer }) => {
+    if (dev) {
+      // Configure Webpack to use filesystem caching instead of memory to prevent heap bloat
+      config.cache = {
+        type: 'filesystem',
+        maxMemoryGenerations: 2, // aggressively garbage collect old module compilations
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
